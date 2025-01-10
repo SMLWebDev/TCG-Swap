6a6e10ae14594e259a80067b862c47dae043b986
Simon <simon@smlwebdevelopment.co.uk>
2025-01-10 16:04:04 +0000

    Worked on Hero and finished Nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -24,19 +24,21 @@
 </script>
 
 <template>
-        <Menubar :model="items" class="w-full">
+        <Menubar :model="items" class="container">
             <template #start>
-                <img src="https://res.cloudinary.com/sml-web-dev/image/upload/v1733502619/tcg-swap-logo.svg" alt="TCG-Logo">
+                <div class="image-container">
+                    <a href="/">
+                        <img src="https://res.cloudinary.com/sml-web-dev/image/upload/v1733502619/tcg-swap-logo.svg" alt="TCG-Logo">
+                    </a>
+                </div>
             </template>
             <template #item="{ item, props, hasSubmenu }">
                 <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
                     <a v-ripple :href="href" v-bind="props.action" @click="navigate">
-                        <span :class="item.icon" />
                         <span>{{ item.label }}</span>
                     </a>
                 </router-link>
                 <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
-                    <span :class="item.icon" />
                     <span>{{ item.label }}</span>
                     <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
                 </a>
@@ -48,6 +50,6 @@
 
 </script>
 
-<style>
-
+<style lang="scss">
+@use '../assets/sass/components/navigation/navbar.scss';
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,22 @@
 
 <template>
   <main>
-    <h1>Hello</h1>
+    <div class="hero">
+      <div class="hero-image">
+        <img src="https://res.cloudinary.com/sml-web-dev/image/upload/v1734192207/two-people-cards_ziawob.png" alt="Playing TCG">
+      </div>
+
+      <div class="hero-content">
+        <div class="hero-container">
+          <h4>Welcome To</h4>
+          <h1>TCG Swap</h1>
+          <p>The best place to make your trades</p>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
+
+<style lang="scss">
+@use '../assets/sass/views/homeview.scss';
+</style>
