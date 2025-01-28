cf9b738d587451e3a259f413d03a1a00b9fdfd3b
Simon <simon@smlwebdevelopment.co.uk>
2025-01-28 17:42:09 +0000

    Finished creating a search function for all cards. Began work on pagination

--- src/components/pokemonview/pokemonCards.vue
+++ src/components/pokemonview/pokemonCards.vue
@@ -8,21 +8,34 @@ pokemonStore.getPokemon();
 </script>
 
 <template>
-   <div class="container">
+   <div class="container pokemon-cards-container">
 
-      <input 
-         type="text" 
-         v-model="pokemonStore.theQuery"
-         id="search"
-         name="search"
-         class="search__input"
-         placeholder="Search..."
-      >
+      <div class="search-results">
+         <div class="search-bar">
+            <input 
+               type="text" 
+               v-model="pokemonStore.theQuery"
+               id="search"
+               name="search"
+               class="search__input"
+               placeholder="Search..."
+            >
+            <button @click="searchQuery = pokemonStore.searchPokemon(pokemonStore.theQuery)"><i class="pi pi-search"></i></button>
+         </div>
+         <div class="pokemon-cards">
+            <div v-for="p in pokemonStore.searchResult">
+               <img :src=p.images.large :alt=p.name>
+            </div>
+         </div>
+      </div>
 
       <div class="cards-container">
 
+         <button @click="pokemonStore.nextPage()">Next</button>
+         <p>{{ pokemonStore.page }}</p>
+
          <div class="pokemon-cards">
-            <div v-for="p in pokemonStore.searched">
+            <div v-for="p in pokemonStore.pokemons">
                <img :src=p.images.large :alt=p.name>
             </div>
          </div>
--- src/stores/pokemonStore.js
+++ src/stores/pokemonStore.js
@@ -2,45 +2,60 @@ import axios from "axios";
 import { defineStore } from "pinia";
 
 
-// export const usePokemonStore = defineStore("pokemon", {
-//     state: () => ({
-//         pokemons: [],
-//         isLoading: false,
-//         theQuery: '',
-//     }),
-//     getters: {
-//         searched() {
-//             return this.pokemons.filter(p => p.name.includes(this.theQuery));
-//         }
-//     },
-//     actions: {
-//         async getPokemon() {
-//             this.isLoading = true;
-//             const res = await axios.get('http://localhost:3000/data');
-//             this.pokemons = res.data;
-//             this.isLoading = false;
-//         }
-//     }
-// })
 
 export const usePokemonStore = defineStore("pokemon", {
     state: () => ({
         pokemons: [],
+        searchResult: [],
         theQuery: '',
+        page: 1,
     }),
     getters: {
-        searched() {
-            return this.pokemons.filter(p => p.name.includes(this.theQuery));
-        }
     },
     actions: {
+        nextPage() {
+            this.page++;
+        },
         async getPokemon() {
-            const res = await axios.get('https://api.pokemontcg.io/v2/cards', {
+            const res = await axios.get('https://api.pokemontcg.io/v2/cards?page=' + this.page, {
                 headers: {
                     'X-Api-Key': import.meta.env.VITE_POKEMON_API_HEADER,
                 },
             });
             this.pokemons = res.data.data;
+            
+        },
+        async searchPokemon(theQuery) {
+            const res = await axios.get('https://api.pokemontcg.io/v2/cards?q=name:' + theQuery, {
+                header: {
+                    'X-Api-Key': import.meta.env.VITE_POKEMON_API_HEADER,
+                },
+            });
+            this.searchResult = res.data.data;
         }
     }
 })
+
+
+
+// export const usePokemonStore = defineStore("pokemon", {
+//     state: () => ({
+//         pokemons: new Map(),
+//         searchResults: [],
+//         theQuery: '',
+//         page: 1,
+//     }),
+//     getters: {
+//     },
+//     actions: {
+//         async getPokemon() {
+//             const res = await axios.get('http://localhost:3000/data?page=2');
+//             this.pokemons = res.data;
+//         },
+//         async searchPokemon(theQuery) {
+//             const res = await axios.get('http://localhost:3000/data?name=' + theQuery)
+//             this.searchResults = res.data;
+
+//         }
+//     }
+// })
