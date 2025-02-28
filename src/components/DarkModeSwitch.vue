5bebd785c3a2b912826e4ce48a3190c332af33b6
Simon <simon@smlwebdevelopment.co.uk>
2025-02-28 14:35:47 +0000

    Began to re-work files and structure

---
+++ src/components/DarkModeSwitch.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref} from "vue";
+
+const checked = ref(false)
+
+function toggleDarkMode() {
+  document.documentElement.classList.toggle('my-app-dark')
+}
+</script>
+
+<template>
+  <div class="card flex justify-end container mx-auto">
+    <p class="text-xs content-center pr-2">Toggle Dark Mode</p>
+    <ToggleSwitch v-model="checked" @click="toggleDarkMode()">
+      <template #handle="{ checked }">
+        <i class="['!text-xs pi', { 'pi-sun': checked, 'pi-moon': checked }] " />
+      </template>
+    </ToggleSwitch>
+  </div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<script setup>
+  import { ref } from 'vue'
+  import { useRouter } from 'vue-router'
+
+  const router = useRouter()
+
+  const items = ref([
+    {
+      label: 'Home',
+      route: '/',
+    },
+    {
+      label: 'Trader',
+      route: '/trader',
+    },
+    {
+      label: 'Login/Register',
+      route: '/login',
+    }
+  ])
+</script>
+
+<template>
+  <div class="m-2">
+    <Menubar :model="items" class="container mx-auto">
+        <template #start>
+          <img src="../assets/tcg-swap-logo.svg" alt="TCG Swap Logo">
+        </template>
+
+        <template #item="{ item, props }">
+            <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+              <a class="text-xl" v-ripple :href="href" v-bind="props.action" @click="navigate">
+                {{ item.label }}
+              </a>
+            </router-link>
+        </template>
+    </Menubar>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
